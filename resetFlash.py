4dd50e632dff2cebdbd04d8c513bfec15bcb0a23
Gidion Siwi Nugroho <gide_gide@yahoo.com>
2024-02-13 19:23:33 +0700

    add script for flash erase

---
+++ resetFlash.py
@@ -0,0 +1,55 @@
+import os
+import logging
+import serial
+import pathlib
+import sys
+CURRENT_PATH = pathlib.Path(__file__).parent.absolute()
+
+from datetime import datetime, timedelta
+from lib.DLMS_Client.dlms_service.dlms_service import CosemDataType, mechanism
+from lib.DLMS_Client.DlmsCosemClient import DlmsCosemClient, TransportSec
+
+METER_USB_PORT = "COM18"
+
+class commSetting:
+    METER_ADDR = 100
+    CLIENT_NUMBER = 0x73
+    AUTH_KEY = "wwwwwwwwwwwwwwww"
+    GUEK = "30303030303030303030303030303030"
+    GAK = "30303030303030303030303030303030"
+    SYS_TITLE = "4954453030303030"
+    # For HW 5
+    USE_RLRQ = True
+    IS_RLRQ_PROTECTED = True
+
+ser_client = DlmsCosemClient(
+    port=METER_USB_PORT,
+    baudrate=19200,
+    parity=serial.PARITY_NONE,
+    bytesize=serial.EIGHTBITS,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=0.3,
+    inactivity_timeout=10,
+    login_retry=1,
+    meter_addr=commSetting.METER_ADDR,
+    client_nb=commSetting.CLIENT_NUMBER,
+)
+
+ser_client.client_logout()
+login_result = ser_client.client_login(commSetting.AUTH_KEY, mechanism.HIGH_LEVEL)
+print('login result: ',login_result)
+obisSample = '0;0;96;15;0;255'
+valueBefore = ser_client.get_cosem_data(1, obisSample, 2)
+print(f'temp before ({obisSample}): {valueBefore}')
+
+resetResult = ser_client.set_cosem_data(1,'1;1;128;130;3;255',2,22,1)
+print(f'reset flash result: {"SUCCESS" if resetResult==0 else "FAILED"}')
+
+input('\nPlease restart the meter. Press ENTER to continue')
+ser_client.client_logout()
+login_result = ser_client.client_login(commSetting.AUTH_KEY, mechanism.HIGH_LEVEL)
+print('login result: ',login_result)
+valueAfter = ser_client.get_cosem_data(1, obisSample, 2)
+print(f'temp after ({obisSample}): {valueAfter}')
+
+ser_client.client_logout()
