95b4da28e8c9ceba9d21596274be974baf5b1c4b
Gidion Siwi Nugroho <gide_gide@yahoo.com>
2024-04-17 16:13:49 +0700

    Add logger with formatter

--- src/Logger.py
+++ src/Logger.py
@@ -8,11 +8,15 @@ def getLogger(filename) -> logging.Logger:
     logger.setLevel(logging.DEBUG)
 
     # Create console handler with a higher log level
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(funcName)s - %(message)s')
     file_handler = logging.FileHandler(filename)
+    file_handler.setFormatter(formatter)
     file_handler.setLevel(logging.DEBUG)
     
     # Create a formatter and set it for both handlers
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
     console_handler.setLevel(logging.DEBUG)
     
     # Add both handlers to the logger
